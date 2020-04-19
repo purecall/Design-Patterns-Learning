6d3a36864eb4cea5c114d0de8cbf768da1688353
purecallxu <purecallxu@tencent.com>
2020-04-19 22:03:10 +0800

    add Decorator

---
+++ Structural Pattern/Decorator/decorator.go
@@ -0,0 +1,66 @@
+package main
+
+import "fmt"
+
+type Shape interface {
+	Render() string
+}
+
+type Circle struct {
+	Radius float32
+}
+
+func (c *Circle) Render() string {
+	return fmt.Sprintf("Circle of radius %f", c.Radius)
+}
+
+func (c *Circle) Resize(factor float32) {
+	c.Radius *= factor
+}
+
+type Square struct {
+	Side float32
+}
+
+func (s *Square) Render() string {
+	return fmt.Sprintf("Square with side %f", s.Side)
+}
+
+// possible, but not generic enough 不够通用
+type ColoredSquare struct {
+	Square
+	Color string
+}
+
+type ColoredShape struct {
+	Shape Shape
+	Color string
+}
+
+func (c *ColoredShape) Render() string {
+	return fmt.Sprintf("%s has the color %s", c.Shape.Render(), c.Color)
+}
+
+type TransparentShape struct {
+	Shape        Shape
+	Transparency float32
+}
+
+func (t *TransparentShape) Render() string {
+	return fmt.Sprintf("%s has %f transparency", t.Shape.Render(), t.Transparency*100.0)
+}
+
+func main() {
+	circle := Circle{2}
+	fmt.Println(circle.Render())
+	//Circle of radius 2.000000
+
+	redCircle := ColoredShape{&circle, "Red"}
+	fmt.Println(redCircle.Render())
+	//Circle of radius 2.000000 has the color Red
+
+	rhsCircle := TransparentShape{&redCircle, 0.5}
+	fmt.Println(rhsCircle.Render())
+	//Circle of radius 2.000000 has the color Red has 50.000000 transparency
+
+}
